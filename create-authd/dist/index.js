8d2ba7cbecb2ccb5c29904a61d053093af8a4427
nmasi322 <edehdivine042@gmail.com>
2023-11-19 18:03:30 +0100

    feat: python module added 🎉

--- create-authd/dist/index.js
+++ create-authd/dist/index.js
@@ -2,10 +2,10 @@ import path from "node:path";
 import fs from "node:fs";
 import minimist from "minimist";
 import prompts from "prompts";
-import { reset, red, lightBlue, magenta, cyan } from "kolorist";
+import { reset, red, lightBlue, magenta, cyan, white } from "kolorist";
 import { fileURLToPath } from "node:url";
 import spawn from "cross-spawn";
-import { green, yellow } from "kolorist";
+import { blue, green, yellow } from "kolorist";
 // excludes numerical autoconversion of project name by defining that the args
 // non associated with an option ( _ ) needs to be parsed as a string.
 const argv = minimist(process.argv.slice(2), { string: ["_"] });
@@ -42,6 +42,18 @@ const Frameworks = [
                 name: "nodejs-mongo-typescript",
                 display: "Typescript & MongoDB",
                 color: cyan,
+            },
+        ],
+    },
+    {
+        name: "python",
+        display: "Python",
+        color: blue,
+        variants: [
+            {
+                display: "FastAPI",
+                name: "fastapi",
+                color: white,
             },
         ],
     },
@@ -214,27 +226,42 @@ async function initialise() {
         }
     };
     const files = fs.readdirSync(templateDir);
-    for (const file of files.filter((f) => f !== "package.json")) {
-        write(file);
+    if (framework.name === "python") {
+        for (const file of files) {
+            write(file);
+        }
+        const cdProjectName = path.relative(cwd, rootPath);
+        console.log(`\nDone. Now run:\n`);
+        if (rootPath !== cwd) {
+            console.log(`  cd ${cdProjectName.includes(" ") ? `"${cdProjectName}"` : cdProjectName}`);
+        }
+        console.log("  source venv/bin/activate");
+        console.log("  pip3 install -r requirements.txt");
+        console.log("  uvicorn main:app --reload");
     }
-    const pkg = JSON.parse(fs.readFileSync(path.join(templateDir, `package.json`), "utf-8"));
-    pkg.name = packageName || getProjectName();
-    write("package.json", JSON.stringify(pkg, null, 2) + "\n");
-    const cdProjectName = path.relative(cwd, rootPath);
-    console.log(`\nDone. Now go to https://www.useplunk.com/ signup and grab your mailing api keys, then setup your config details`);
-    console.log(`\nThen run:\n`);
-    if (rootPath !== cwd) {
-        console.log(`  cd ${cdProjectName.includes(" ") ? `"${cdProjectName}"` : cdProjectName}`);
-    }
-    switch (pkgManager) {
-        case "yarn":
-            console.log("  yarn");
-            console.log("  yarn dev");
-            break;
-        default:
-            console.log(`  ${pkgManager} install`);
-            console.log(`  ${pkgManager} run dev`);
-            break;
+    else {
+        for (const file of files.filter((f) => f !== "package.json")) {
+            write(file);
+        }
+        const pkg = JSON.parse(fs.readFileSync(path.join(templateDir, `package.json`), "utf-8"));
+        pkg.name = packageName || getProjectName();
+        write("package.json", JSON.stringify(pkg, null, 2) + "\n");
+        const cdProjectName = path.relative(cwd, rootPath);
+        console.log(`\nDone. Now go to https://www.useplunk.com/ signup and grab your mailing api keys, then setup your config details`);
+        console.log(`\nThen run:\n`);
+        if (rootPath !== cwd) {
+            console.log(`  cd ${cdProjectName.includes(" ") ? `"${cdProjectName}"` : cdProjectName}`);
+        }
+        switch (pkgManager) {
+            case "yarn":
+                console.log("  yarn");
+                console.log("  yarn dev");
+                break;
+            default:
+                console.log(`  ${pkgManager} install`);
+                console.log(`  ${pkgManager} run dev`);
+                break;
+        }
     }
     console.log();
 }
